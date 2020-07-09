83c1e91ed31af32ebadbe76782d586f80f1282a1
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-08 21:59:40 -0300

    Criação de rotas e cadastro de funcionários

---
+++ routes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', function(req, res){
+    return res.redirect("/instructors")
+})
+
+routes.get('/instructors', function(req, res){
+    return res.render("instructors/index")
+})
+
+routes.get('/members', function(req, res){
+    return res.send("members")
+})
+
+routes.get('/instructors/create', function(req,res){
+    return res.render('instructors/create')
+})
+
+
+routes.post("/instructors", function(req, res){
+    return res.send("recebido");
+})
+
+module.exports = routes
