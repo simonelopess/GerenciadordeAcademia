b0fe93f01dbbf3be6bc0cd741e8468a6ea431847
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-16 18:23:00 -0300

    Pagination Logic created

--- public/scripts.js
+++ public/scripts.js
@@ -7,3 +7,37 @@ for(item of menuItems){
         item.classList.add("active")
     }
 }
+
+
+//Paginação
+
+//totalPages = 20
+//selectedPage = 15
+
+
+let totalPages = 20, 
+    selectedPage = 15,
+    pages = [],
+    odlPage
+
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage ++ ){
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage>=selectedPage - 2
+
+    if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
+        
+
+        if(odlPage && currentPage - odlPage> 2){
+            pages.push('...')
+        }
+        if(odlPage && currentPage - odlPage == 2){
+            pages.push(odlPage + 1)
+        }
+
+        pages.push(currentPage)
+        odlPage = currentPage
+    }
+}
+
