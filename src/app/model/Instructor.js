f8646edf7ab66f2a55f50c1551f9c3b88fdf34ea
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-16 11:22:01 -0300

    Query total_students created

--- src/app/model/Instructor.js
+++ src/app/model/Instructor.js
@@ -3,7 +3,11 @@ const db = require('../../config/db')
 
 module.exports = {
     all(callback){
-        db.query(`SELECT * from instructors`, function(err, results){
+        db.query(`SELECT instructors.*, count(members) AS total_students
+         FROM instructors 
+         LEFT JOIN members ON (instructors.id = members.instructor_id)
+         GROUP BY instructors.id   
+         ORDER BY total_students DESC`, function(err, results){
             if(err) throw "Database Error"
 
             callback(results.rows)
