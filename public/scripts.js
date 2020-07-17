4e05d3899e8f621bf73230a1acc077b079c43c5c
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-16 21:36:05 -0300

    Style front finished

--- public/scripts.js
+++ public/scripts.js
@@ -42,21 +42,35 @@ function paginate(selectedPage, totalPages){
     return pages
 }
 
-const pagination = document.querySelector(".pagination");
-const page = +pagination.dataset.page
-const total =  +pagination.dataset.total //+ - transforma em número
-const pages = paginate(page, total)
+function createPagination(pagination){
 
-
-let elements = ""
-
-for (let page of pages){
-    if(String(page.includes("..."))){
-        elements += `<span>${page}</span>`
-    }else{
-        elements += `<a href="?page=${page}">${page}</a>`
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total =  +pagination.dataset.total //+ - transforma em número
+    const pages = paginate(page, total)
+    
+    
+    let elements = ""
+    
+    for (let page of pages){
+        if(String(page).includes("...")){
+            elements += `<span>${page}</span>`
+        }else{
+            if(filter){
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            }else{
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
+        }
     }
+    
+    
+    pagination.innerHTML = elements
 }
 
+const pagination = document.querySelector(".pagination");
 
-pagination.innerHTML = elements
+
+if(pagination){
+    createPagination(pagination)
+}
